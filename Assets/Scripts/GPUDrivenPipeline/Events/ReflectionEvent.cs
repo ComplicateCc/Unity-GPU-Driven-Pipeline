788965bd58a940bb65e86ccdaf79297f1aff13e4
Maxwell <gengy@msoe.edu>
2019-02-16 11:00:26 -0600

    backup

--- Assets/Scripts/GPUDrivenPipeline/Events/ReflectionEvent.cs
+++ Assets/Scripts/GPUDrivenPipeline/Events/ReflectionEvent.cs
@@ -1,7 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Experimental.Rendering;
 using UnityEngine;
-
-public class ReflectionEvent
+namespace MPipeline
 {
+    [CreateAssetMenu(menuName = "GPURP Events/Reflection")]
+    [RequireEvent(typeof(LightingEvent))]
+    public class ReflectionEvent : PipelineEvent
+    {
+        public override bool CheckProperty()
+        {
+            return true;
+        }
+        protected override void Init(PipelineResources resources)
+        {
+            
+        }
+        protected override void Dispose()
+        {
+
+        }
+
+        public override void FrameUpdate(PipelineCamera cam, ref PipelineCommandData data)
+        {
+            
+        }
+    }
 }
